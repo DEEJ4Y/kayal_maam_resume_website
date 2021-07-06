80db319ea32ecc0236ac7b42f81af4ee3f166b28
davidjosephind <davidjosephind@gmail.com>
2021-07-06 17:07:38 +0530

    Update Carousel to remove warning

--- components/Index/Publications/Carousel/Carousel.jsx
+++ components/Index/Publications/Carousel/Carousel.jsx
@@ -8,11 +8,7 @@ const PublicationsCarousel = () => {
     <Carousel interval={7000}>
       {publications.map((publication, idx) => {
         // console.log(publication, idx);
-        return idx === 0 ? (
-          <Carousel.Item key={idx} active>
-            <CarouselItem publication={publication} />
-          </Carousel.Item>
-        ) : (
+        return (
           <Carousel.Item key={idx}>
             <CarouselItem publication={publication} />
           </Carousel.Item>
