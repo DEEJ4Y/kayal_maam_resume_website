7d1f63c337d6d7228733a82dfeac9f03bdbcea14
davidjosephind <davidjosephind@gmail.com>
2021-07-07 17:11:10 +0530

    Add achievements page

--- components/Index/ActivitiesAndAchievements/ActivitiesAndAchievements.jsx
+++ components/Index/ActivitiesAndAchievements/ActivitiesAndAchievements.jsx
@@ -2,6 +2,8 @@ import styles from "./ActivitiesAndAchievements.module.css";
 
 import AchievementsCarousel from "./AchievementsCarousel/AchievementsCarousel";
 
+import Link from "next/link";
+
 const ActivitiesAndAchievements = () => {
   return (
     <div className="container-padding bg-theme text-theme">
@@ -9,7 +11,12 @@ const ActivitiesAndAchievements = () => {
         <h2 className={styles["heading"]}>
           Professional Activities and Achievements
         </h2>
-        <p>Have a look at my activities and achievements here.</p>
+        <p>
+          Have a look at my activities and achievements{" "}
+          <Link passHref href="/achievements">
+            here.
+          </Link>
+        </p>
         <AchievementsCarousel />
       </div>
     </div>
---
+++ pages/achievements.jsx
@@ -0,0 +1,32 @@
+import HeadTags from "../components/Index/Head/HeadTags";
+import Navbar from "../components/Index/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import activiesAndAchievements from "../lib/index/activitiesAndAchievements";
+
+const PublicationsPage = () => {
+  return (
+    <div>
+      <HeadTags />
+      <Navbar />
+      <main className="container-padding bg-accent text-accent">
+        <div className="container-fluid">
+          <h2 style={{ fontWeight: 800, marginBottom: "2rem" }}>
+            Professional Activities and Achievements
+          </h2>
+          <ul>
+            {activiesAndAchievements.map(({ text }, idx) => {
+              return (
+                <li key={idx}>
+                  <h5>{text}</h5>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default PublicationsPage;
