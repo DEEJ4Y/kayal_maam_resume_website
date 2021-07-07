fcbbc62fb03668903bc5bda0d09d906281fc8616
davidjosephind <davidjosephind@gmail.com>
2021-07-07 17:01:58 +0530

    Link homepage to publications page and back

--- components/Index/Navbar/Navbar.jsx
+++ components/Index/Navbar/Navbar.jsx
@@ -3,14 +3,18 @@ import styles from "./navbar.module.css";
 import Toggler from "./Toggler/Toggler";
 import TogglerDropdownList from "./TogglerDropdownList/TogglerDropdownList";
 
+import Link from "next/link";
+
 const Navbar = () => {
   const [togglerStateOpen, setTogglerStateOpen] = useState(false);
   return (
     <nav className={`navbar bg-theme text-theme ${styles["navbar-shadow"]}`}>
       <div className="container-fluid">
-        <a className={`navbar-brand ${styles["brand-title"]}`}>
-          KayalvizhiJayavel
-        </a>
+        <Link passHref href="/">
+          <span className={`navbar-brand ${styles["brand-title"]}`}>
+            KayalvizhiJayavel
+          </span>
+        </Link>
         <Toggler
           onClick={() => {
             togglerStateOpen
--- components/Index/Publications/Publications.jsx
+++ components/Index/Publications/Publications.jsx
@@ -2,14 +2,19 @@ import styles from "./Publications.module.css";
 
 import PublicationsCarousel from "./Carousel/Carousel";
 
+import Link from "next/link";
+
 const Publications = () => {
   return (
     <div className="bg-theme text-theme container-padding">
       <div className="container-fluid">
         <h2 className={styles["heading"]}>Publications</h2>
         <p className={styles["text-size"]}>
-          Only a few of my publications are listed here. For more details visit
-          my Google Scholar page and my Scopus page.
+          Only a few of my publications are listed{" "}
+          <Link passHref href="/publications">
+            here
+          </Link>
+          . For more details visit my Google Scholar page and my Scopus page.
         </p>
         <PublicationsCarousel />
       </div>
